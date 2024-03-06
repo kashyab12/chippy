57bb921901d28e8663da2bd157f7e418c0fc08fc
kashyaba <iamkashyab@gmail.com>
2024-03-05 23:55:05 -0800

    added adminRouter to handle metrics html page.

--- main.go
+++ main.go
@@ -20,9 +20,9 @@ func (cfg *apiConfig) metricsMiddleware(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) fsHitsHandler(writer http.ResponseWriter, _ *http.Request) {
 	log.Println("fsHitsHandler ep hit!")
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 	writer.WriteHeader(http.StatusOK)
-	hitsString := fmt.Sprintf("Hits: %d", cfg.fsHits)
+	hitsString := fmt.Sprintf("<html>\n\n<body>\n<h1>Welcome, Chirpy Admin</h1>\n<p>Chirpy has been visited %d times!</p>\n</body>\n\n</html>\n", cfg.fsHits)
 	_, err := writer.Write([]byte(hitsString))
 	if err != nil {
 		log.Fatal(err)
@@ -63,14 +63,16 @@ func main() {
 	const port = 8080
 	appRouter := chi2.NewRouter()
 	apiRouter := chi2.NewRouter()
+	adminRouter := chi2.NewRouter()
 	apiCfg := apiConfig{fsHits: 0}
 	fsHandler := apiCfg.metricsMiddleware(http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
 	appRouter.Handle("/app/*", fsHandler)
 	appRouter.Handle("/app", fsHandler)
 	apiRouter.Get("/healthz", readinessEndpoint)
-	apiRouter.Get("/metrics", apiCfg.fsHitsHandler)
 	apiRouter.HandleFunc("/reset", apiCfg.resetFsHitsHandler)
+	adminRouter.Get("/metrics", apiCfg.fsHitsHandler)
 	appRouter.Mount("/api", apiRouter)
+	appRouter.Mount("/admin", adminRouter)
 	corsMux := corsMiddleware(appRouter)
 	server := http.Server{
 		Handler: corsMux,

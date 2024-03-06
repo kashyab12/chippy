2f586c8a71c15b93bb9760ecd43ad3596e7a5ac6
kashyaba <iamkashyab@gmail.com>
2024-03-05 22:40:39 -0800

    metrics and reset init done :D

--- main.go
+++ main.go
@@ -17,15 +17,22 @@ func (cfg *apiConfig) metricsMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func (cfg *apiConfig) fsHitsHandler(writer http.ResponseWriter, request *http.Request) {
+func (cfg *apiConfig) fsHitsHandler(writer http.ResponseWriter, _ *http.Request) {
 	log.Println("fsHitsHandler ep hit!")
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
 	hitsString := fmt.Sprintf("Hits: %d", cfg.fsHits)
 	_, err := writer.Write([]byte(hitsString))
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+}
+
+func (cfg *apiConfig) resetFsHitsHandler(writer http.ResponseWriter, _ *http.Request) {
+	log.Println("resetFsHandler ep hit!")
+	cfg.fsHits = 0
+	writer.WriteHeader(http.StatusOK)
 }
 
 func corsMiddleware(next http.Handler) http.Handler {
@@ -56,6 +63,8 @@ func main() {
 			return
 		}
 	})
+	mux.HandleFunc("/metrics/", apiCfg.fsHitsHandler)
+	mux.HandleFunc("/reset/", apiCfg.resetFsHitsHandler)
 	corsMux := corsMiddleware(mux)
 	server := http.Server{
 		Handler: corsMux,

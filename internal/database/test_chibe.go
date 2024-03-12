45f094a220942c57217175c620da51f1f5120ab3
kashyaba <iamkashyab@gmail.com>
2024-03-12 00:51:58 -0700

    Get rid of TestCreateChirp; don't need to worry about tests since suite is present

--- internal/database/test_chibe.go
+++ internal/database/test_chibe.go
@@ -10,15 +10,3 @@ func TestNewDb() (*DB, error) {
 	log.Printf("Got the man chibee at %v\n", chibe.path)
 	return chibe, err
 }
-
-func TestCreateChirp() {
-	chibe, _ := TestNewDb()
-	chirps := []string{"chibeeeee the deeeebeeee :DD::D::D::D::D::DD:", "cool", "awesome"}
-	for _, chirp := range chirps {
-		newChirp, err := chibe.CreateChirp(chirp)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("the new chirp: %v\n", newChirp)
-	}
-}

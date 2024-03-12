03ab01d06cf7a212febb6457dabf1836ac7d6e02
kashyaba <iamkashyab@gmail.com>
2024-03-12 00:45:13 -0700

    include authorId within the chirp struct

--- internal/database/chibe.go
+++ internal/database/chibe.go
@@ -28,8 +28,9 @@ type DBStructure struct {
 }
 
 type Chirp struct {
-	Uid  int    `json:"id"`
-	Body string `json:"body"`
+	Uid      int    `json:"id"`
+	Body     string `json:"body"`
+	AuthorID int    `json:"author_id"`
 }
 
 type User struct {

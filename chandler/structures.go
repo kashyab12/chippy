5493eb00728ec7d1c700bfb3ba670761ed41e747
kashyaba <iamkashyab@gmail.com>
2024-03-12 22:12:42 -0700

    Added DeleteChirp handler, tests passed :)

--- chandler/structures.go
+++ chandler/structures.go
@@ -54,6 +54,7 @@ func GetApiRouter(config *ApiConfig) *chi2.Mux {
 	apiRouter.HandleFunc("/reset", config.resetFsHitsHandler)
 	apiRouter.Get("/chirps", GetChirp)
 	apiRouter.Get("/chirps/{chirpID}", GetSingleChirp)
+	apiRouter.Delete("/chirps/{chirpID}", config.DeleteChirp)
 	apiRouter.Post("/chirps", config.PostChirp)
 	apiRouter.Post("/users", PostUsers)
 	apiRouter.Put("/users", config.PutUser)
